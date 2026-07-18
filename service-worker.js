b4e67aee64a8df60a42641411b78555f178505f4
venia5454 <Veniamin.mosiichuk@icloud.com>
2026-07-18 11:51:02 -0700

    Add service worker for file caching
    Implement service worker for caching files.

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "expense-tracker-v1";
+
+const files = [
+  "index.html",
+  "style.css",
+  "script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(files))
+  );
+});
+
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
